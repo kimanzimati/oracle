0dc89cdbeee161510aee2831d8750721f0770a59
Kimanzi Mati <kimanzimati@hotmail.com>
2022-07-07 12:06:36 +0300

    Include POST request

--- services/web-server.js
+++ services/web-server.js
@@ -15,6 +15,11 @@ function initialize() {
       // Combines logging info from request and response
       app.use(morgan('combined'));
 
+      // Parse incoming JSON requests and revive JSON.
+    app.use(express.json({
+      reviver: reviveJson
+    }));
+
       // *** app.get call below this line ***
       
    // app.get('/', (req, res) => {
@@ -63,3 +68,14 @@ function close() {
   }
   
   module.exports.close = close;
+
+  const iso8601RegExp = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/;
+
+function reviveJson(key, value) {
+  // revive ISO 8601 date strings to instances of Date
+  if (typeof value === 'string' && iso8601RegExp.test(value)) {
+    return new Date(value);
+  } else {
+    return value;
+  }
+}
